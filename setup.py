b2c97d9c82aee79c229c75faa791bded3e1418ec
Tomas Drencak <tomas@drencak.com>
2012-06-22 13:17:24 +0200

    setup.py correction

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     version='0.1',
     author='Tomas Drencak',
     packages=find_packages(),
-    py_modules= ['emailactivation',],
     install_requires=['itsdangerous', 'simplejson', 'Flask-Mail'],
     tests_require=tests_require,
     extras_require={'test': tests_require}
