ff633bbd1f7b849453be65263bb9c9b9682e5e9e
Tomas Drencak <tomas@drencak.com>
2012-10-12 23:45:11 +0300

    Exclude tests from packaging

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='flask-emailactivation',
     version='0.1',
     author='Tomas Drencak',
-    packages=find_packages(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=['itsdangerous', 'simplejson', 'Flask-Mail'],
     tests_require=tests_require,
     extras_require={'test': tests_require}
